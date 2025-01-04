0e2ad1995c8f115eabe2f1795d361d7a87a522bd
Gavin Park <gpark@hotmail.com>
2025-01-04 10:20:40 +0000

    update command

--- src/TestApi/Application/Queries/Contacts/GetContactByIdResponse.cs
+++ src/TestApi/Application/Queries/Contacts/GetContactByIdResponse.cs
@@ -6,22 +6,21 @@ namespace TestApi.Application.Queries.Contacts
     public class GetContactByIdResponse
     {
         public Guid Id { get; set; }
-        public Company ContactCompany { get; set; }
 
-        //public string Name { get; set; }
+        public string Name { get; set; }
 
-        //public string Email { get; set; }
+        public string Email { get; set; }
 
-        //public string Phone { get; set; }
+        public string Phone { get; set; }
 
-        //public string Address { get; set; }
+        public string Address { get; set; }
 
-        //public string City { get; set; }
+        public string City { get; set; }
 
-        //public string Region { get; set; }
+        public string Region { get; set; }
 
-        //public string PostalCode { get; set; }
-        
-        //public Dictionary<Guid, Appointment> Appointments { get; set; } = new Dictionary<Guid, Appointment>();
+        public string PostalCode { get; set; }
+
+        public Dictionary<Guid, Appointment> Appointments { get; set; } = new Dictionary<Guid, Appointment>();
     }
 }
